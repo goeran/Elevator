98567c1c54fb8ebe6f30dc263ae003c69640597b
goeran <mail@goeran.no>
2012-09-27 14:13:55 +0200

    rigged up to write acceptance tests against the console

---
+++ Elevator.AcceptanceTests/ConsoleTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using NUnit.Framework;
+
+namespace Elevator.AcceptanceTests
+{
+    class ConsoleTests
+    {
+        [TestFixture]
+        public class When_running
+        {
+            [Test]
+            public void bla()
+            {
+                var process = new Process();
+                process.StartInfo.WorkingDirectory = Environment.CurrentDirectory;
+                process.StartInfo.FileName = "Elevator.exe";
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+
+                process.Start();
+                
+                using (var sr = process.StandardOutput)
+                {
+                    Console.WriteLine(sr.ReadToEnd());
+                }
+            }
+        }
+
+    }
+}
--- Elevator/Program.cs
+++ Elevator/Program.cs
@@ -9,6 +9,7 @@ namespace Elevator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello world!");
         }
     }
 }
